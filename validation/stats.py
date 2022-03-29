e26087f29cf1e325da924ff402259a4bb5dad3ae
Arjan Draisma <neolook@protonmail.com>
2022-03-29 09:11:35 +0200

    split get metrics

--- validation/stats.py
+++ validation/stats.py
@@ -1,3 +1,18 @@
+def get_metrics(truth_positive, truth_negative, prediction_positive, prediction_negative):
+    """Generates the tp/fp/tn/fn metrics for validation from a set of truth pairs and prediction pairs"""
+    missing_positives = truth_positive - prediction_positive
+    missing_negatives = truth_negative - prediction_negative
+
+    true_positive = truth_positive & prediction_positive
+    TP = len(true_positive)
+    false_positive = truth_positive & prediction_negative
+    FP = len(false_positive)
+    true_negative = truth_negative & prediction_negative.symmetric_difference(missing_positives)
+    TN = len(true_negative)
+    false_negative = truth_negative & prediction_negative.symmetric_difference(missing_negatives)
+    FN = len(false_negative)
+    return TP, FP, TN, FN
+
 def print_confusion_matrix(truth, predictions):
     """Prints a confusion matrix with neat statistics for validation
     
@@ -9,18 +24,13 @@ def print_confusion_matrix(truth, predictions):
     """
     true_pairs_under_treshold, true_pairs_over_treshold = truth
     pred_pairs_under_treshold, pred_pairs_over_treshold = predictions
-    
-    missing_positives = true_pairs_under_treshold - pred_pairs_under_treshold
-    missing_negatives = true_pairs_over_treshold - pred_pairs_over_treshold
 
-    true_positive = true_pairs_under_treshold & pred_pairs_under_treshold
-    TP = len(true_positive)
-    false_positive = true_pairs_under_treshold & pred_pairs_over_treshold
-    FP = len(false_positive)
-    true_negative = true_pairs_over_treshold & pred_pairs_over_treshold.symmetric_difference(missing_positives)
-    TN = len(true_negative)
-    false_negative = true_pairs_over_treshold & pred_pairs_over_treshold.symmetric_difference(missing_negatives)
-    FN = len(false_negative)
+    TP, FP, TN, FN = get_metrics(
+        true_pairs_under_treshold,
+        true_pairs_over_treshold,
+        pred_pairs_under_treshold,
+        pred_pairs_over_treshold
+    )
 
     print("Total pairs in truth: ", len(truth[0]) + len(truth[1]))
     print("Total pairs in prediction: ", len(predictions[0]) + len(predictions[1]))
